1516fbb45921f11fe226ed9956497f1349d97dcf
Carlos Eduardo Merino Ventura <merinoventura123@gmail.com>
2025-06-08 12:40:56 -0600

    Modificaciones de tuto
    Modificaciones de tuto

---
+++ src/main/java/org/example/springbootstart/OrderService.java
@@ -0,0 +1,9 @@
+package org.example.springbootstart;
+
+public class OrderService {
+    public void placeOrder(){
+        var paymentService = new StripePaymentService();
+        paymentService.processPayment(10);
+
+    }
+}
---
+++ src/main/java/org/example/springbootstart/StripePaymentService.java
@@ -0,0 +1,8 @@
+package org.example.springbootstart;
+
+public class StripePaymentService {
+    public void processPayment(double amount){
+        System.out.println("Stripe");
+        System.out.println("Amout "+amount);
+    }
+}
